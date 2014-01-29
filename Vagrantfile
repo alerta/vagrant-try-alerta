ddcd8e4b0a0e747b7e3df493986be4e34172ebc9
Nick Satterly <nick.satterly@guardian.co.uk>
2014-01-29 15:54:29 +0000

    change to use setup.sh instead of puppet

--- Vagrantfile
+++ Vagrantfile
@@ -3,10 +3,10 @@
 
 Vagrant.configure("2") do |config|
 
-  config.vm.box = "precise64"
-  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.vm.box = "raring"
+  config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/raring/current/raring-server-cloudimg-amd64-vagrant-disk1.box"
 
-  config.vm.network :private_network, ip: "192.168.33.11"
+  config.vm.network :private_network, ip: "192.168.33.15"
 
   config.vm.network :forwarded_port, guest: 80, host: 8000      # alerta-dashboard
   config.vm.network :forwarded_port, guest: 8080, host: 8080    # alerta-api
@@ -14,9 +14,6 @@ Vagrant.configure("2") do |config|
   config.vm.network :forwarded_port, guest: 4567, host: 4567    # riemann-dash
   config.vm.network :forwarded_port, guest: 5556, host: 5556    # riemann-ws
 
-  config.vm.provision :puppet do |puppet|
-    puppet.manifests_path = "manifests"
-    puppet.manifest_file  = "site.pp"
-  end
+  config.vm.provision :shell, :path => "setup.sh"
 
 end
---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -ex
+
+GIT_REPO=alerta/vagrant-try-alerta
+
+# Update packages
+sudo apt-get update
+
+# Install required dependencies
+sudo apt-get install -y git wget python-setuptools python-pip
+sudo apt-get install -y build-essential python-dev
+
+# Install and configure MongoDB
+sudo apt-get install -y mongodb-server
+echo "smallfiles = true" | sudo tee -a /etc/mongodb.conf
+
+# Install and configure RabbitMQ
+sudo apt-get install -y rabbitmq-server
+sudo /usr/lib/rabbitmq/bin/rabbitmq-plugins enable rabbitmq_management
+sudo /usr/lib/rabbitmq/bin/rabbitmq-plugins enable rabbitmq_stomp
+sudo service rabbitmq-server restart
+cd /var/tmp
+wget http://guest:guest@localhost:55672/cli/rabbitmqadmin && chmod +x rabbitmqadmin
+./rabbitmqadmin declare exchange name=alerts type=fanout
+
+# Install and configure Alerta
+sudo pip install alerta
+mkdir -p /etc/alerta
+wget -O /etc/alerta/alerta.conf https://raw.github.com/${GIT_REPO}/master/files/alerta.conf
+wget -O /etc/init/alerta.conf https://raw.github.com/${GIT_REPO}/master/files/upstart-alerta.conf
+initctl reload-configuration alerta
+
+# Install and configure Apache web server
+sudo apt-get install -y apache2 libapache2-mod-wsgi
+mkdir -p /var/www/alerta
+wget -O /var/www/alerta/alerta-api.wsgi https://raw.github.com/${GIT_REPO}/master/files/alerta-api.wsgi
+wget -O /var/www/alerta/alerta-dashboard.wsgi https://raw.github.com/${GIT_REPO}/master/files/alerta-dashboard.wsgi
+wget -O /etc/apache2/conf.d/alerta-api.conf https://raw.github.com/${GIT_REPO}/master/files/httpd-alerta-api.conf
+wget -O /etc/apache2/conf.d/alerta-dashboard.conf https://raw.github.com/${GIT_REPO}/master/files/httpd-alerta-dashboard.conf
+apachectl graceful
+
