d3a1e23aac0a699e13a61fa73fcdec97b93a75c4
Nick Satterly <nick.satterly@guardian.co.uk>
2014-02-06 00:38:56 +0000

    add example riemann tags

--- scripts/riemann.sh
+++ scripts/riemann.sh
@@ -23,4 +23,4 @@ sudo apt-get -y install libxslt-dev libxml2-dev
 sudo gem install nokogiri -v '1.5.9' --no-ri --no-rdoc
 
 sudo gem install riemann-tools --no-ri --no-rdoc
-riemann-health --host 127.0.0.1 &
+riemann-health --host 127.0.0.1 --tag location=london --tag os=linux &
