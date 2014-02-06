0d2a43de98012bb1cf3e39860ea46cb325c56cad
Nick Satterly <nick.satterly@guardian.co.uk>
2014-02-06 18:52:30 +0000

    add nagios broker heartbeat config

--- scripts/nagios3.sh
+++ scripts/nagios3.sh
@@ -12,7 +12,9 @@ apt-get -y install nagios3 nagios-nrpe-plugin libcurl4-openssl-dev
 git clone https://github.com/alerta/nagios3-alerta.git
 cd nagios3-alerta
 make && make install
-
 echo "broker_module=/usr/lib/nagios3/alerta-neb.o http://localhost:8080 debug=1" | tee -a /etc/nagios3/nagios.cfg
+
+cd /etc/nagios3/conf.d
+wget https://raw.github.com/alerta/nagios3-alerta/master/config/nagios3-heartbeat.cfg
 service nagios3 restart
 
