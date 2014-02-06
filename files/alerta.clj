e65cb7c63da7b2e971bb5e699c1eb90cac52180a
Nick Satterly <nick.satterly@guardian.co.uk>
2014-02-06 00:37:33 +0000

    update riemann to work with key-value tags

--- files/alerta.clj
+++ files/alerta.clj
@@ -22,6 +22,11 @@
 		; :debug-body true
                 :throw-entire-message? true})))
 
+(defn key-value-split
+  "Split on equals, always returning two values."
+  [s]
+  (if (.contains s "=") (clojure.string/split s #"=" 2) [s ""]))
+
 (defn format-alerta-event
   "Formats an event for Alerta."
   [event]
@@ -38,7 +43,7 @@
    :severity (:state event)
    :environment [(get event :environment "INFRA")]
    :service [(get event :grid "Common")]
-   :tags (:tags event)
+   :tags (into {} (map #(key-value-split %) (:tags event)))
    :text (:description event)
    :moreInfo "more info here"
    :rawData event})
