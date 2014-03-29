e18fba3cb31d063fa482cfb295b7bf354de3e405
Nick Satterly <nick.satterly@guardian.co.uk>
2014-03-29 23:57:04 +0000

    update alerta.sh

--- scripts/alerta.sh
+++ scripts/alerta.sh
@@ -12,39 +12,31 @@ service mongodb restart
 
 # Configure RabbitMQ
 /usr/lib/rabbitmq/bin/rabbitmq-plugins enable rabbitmq_management
-/usr/lib/rabbitmq/bin/rabbitmq-plugins enable rabbitmq_stomp
 service rabbitmq-server restart
-wget -qO /var/tmp/rabbitmqadmin http://guest:guest@localhost:55672/cli/rabbitmqadmin && chmod +x /var/tmp/rabbitmqadmin
-/var/tmp/rabbitmqadmin declare exchange name=alerts type=fanout durable=true
 
 # Install and configure Alerta
 pip install alerta
-mkdir -p /etc/alerta
-cp /vagrant/files/alerta.conf /etc/alerta/alerta.conf
-cp /vagrant/files/upstart-alerta.conf /etc/init/alerta.conf
-initctl reload-configuration alerta
-service alerta restart
+groupadd alerta
+useradd -g alerta alerta
 
 # Configure Apache web server
-mkdir -p /var/www/alerta
-cp /vagrant/files/alerta-api.wsgi /var/www/alerta/alerta-api.wsgi
-cp /vagrant/files/httpd-alerta-api.conf /etc/apache2/sites-available/alerta-api
-a2ensite alerta-api
+wget -qO /etc/apache2/sites-available/alerta https://raw.githubusercontent.com/guardian/alerta/master/etc/httpd-alerta.conf
+# FIXME - points to wsgi file in /opt/alerta...
+# FIXME - wsgi file tries to activate virtualenv
+a2ensite alerta
 
-cp /vagrant/files/alerta-dashboard.wsgi /var/www/alerta/alerta-dashboard.wsgi
-cp /vagrant/files/httpd-alerta-dashboard.conf /etc/apache2/sites-available/alerta-dashboard
-PYTHON_ROOT_DIR=`python -c "import alerta; print(alerta.__dict__['__path__'][0])"`
-sed -i "s#@STATIC@#$PYTHON_ROOT_DIR#" /etc/apache2/sites-available/alerta-dashboard
-a2ensite alerta-dashboard
+#cp /vagrant/files/alerta-dashboard.wsgi /var/www/alerta/alerta-dashboard.wsgi
+#cp /vagrant/files/httpd-alerta-dashboard.conf /etc/apache2/sites-available/alerta-dashboard
+#PYTHON_ROOT_DIR=`python -c "import alerta; print(alerta.__dict__['__path__'][0])"`
+#sed -i "s#@STATIC@#$PYTHON_ROOT_DIR#" /etc/apache2/sites-available/alerta-dashboard
+#a2ensite alerta-dashboard
 
-chmod 0777 /var/log/alerta && chgrp www-data /var/log/alerta
 a2dissite 000-default
 echo "ServerName localhost" >> /etc/apache2/apache2.conf
-service apache2 reload
+apachectl restart
 
 # Generate test alerts
-cp /vagrant/files/create-alerts.sh /var/tmp/create-alerts.sh
-chmod +x /var/tmp/create-alerts.sh && /var/tmp/create-alerts.sh
+wget -qO - /var/tmp/create-alerts.sh https://raw.githubusercontent.com/guardian/alerta/master/contrib/examples/create-new-alert.sh | sh
 
 # Clean-up
 mongo monitoring --eval 'db.heartbeats.remove()'
