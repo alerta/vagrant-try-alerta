e365016f2e67ea2c2506e10c7bae77036d22f962
Nick Satterly <nfsatterly@gmail.com>
2018-10-30 01:47:31 +0100

    wip

--- scripts/ubuntu/opennms.sh
+++ scripts/ubuntu/opennms.sh
@@ -36,11 +36,66 @@ SKIP_IPLIKE_INSTALL=1 apt-get install default-mta opennms -y
 
 /usr/share/opennms/bin/install -dis
 
+pip install git+https://github.com/alerta/alerta-contrib.git#subdirectory=integrations/syslog
+
+export SYSLOG_TCP_PORT=1514
+export SYSLOG_UDP_PORT=1514
+
+cat >/etc/systemd/system/alerta-syslog.service <<EOF
+[Unit]
+Description=Alerta Syslog Listener
+After=network.target
+
+[Service]
+ExecStart=/opt/alerta/bin/alerta-syslog
+Type=simple
+
+[Install]
+WantedBy=default.target
+EOF
+
+cat >/etc/opennms/syslog-northbounder-configuration.xml <<EOF
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<syslog-northbounder-config>
+   <!-- The following are set to the default values. -->
+   <enabled>true</enabled>
+   <nagles-delay>1000</nagles-delay>
+   <batch-size>100</batch-size>
+   <queue-size>300000</queue-size>
+   <!-- <message-format>ALARM ID:${alarmId} NODE:${nodeLabel}; ${logMsg}</message-format> -->
+    <!-- You can specify date format within <date-format>, default is ISO 8601 -->
+   <message-format>ALARM ID:${alarmId} NODE:${nodeLabel} IP:${ipAddr}
+      FIRST:${firstOccurrence} LAST:${lastOccurrence}
+      COUNT:${count} UEI:${alarmUei} SEV:${severity}
+      x733Type:${x733AlarmType} x733Cause:${x733ProbableCause}
+      ${logMsg}
+   </message-format>
+<!-- More than one destination is supported -->
+   <destination>
+      <destination-name>localTest</destination-name>
+      <host>127.0.0.1</host>
+      <port>1514</port>
+      <ip-protocol>UDP</ip-protocol>
+      <facility>LOCAL0</facility>
+      <max-message-length>1024</max-message-length>
+      <send-local-name>true</send-local-name>
+      <send-local-time>true</send-local-time>
+      <truncate-message>false</truncate-message>
+   </destination>
+<!-- Highly recommended, but not required, to only forward a set of Alarm UEIs -->
+<!--
+   <uei>uei.opennms.org/nodes/nodeDown</uei>
+   <uei>uei.opennms.org/nodes/nodeUp</uei>
+-->
+</syslog-northbounder-config>
+EOF
+
+systemctl start alerta-syslog
 systemctl start opennms
 
-ufw --force enable
-ufw allow 8980
-ufw status
+# ufw --force enable
+# ufw allow 8980
+# ufw status
 
 echo "http://192.168.0.121:8980/opennms"
 echo "admin/admin"
